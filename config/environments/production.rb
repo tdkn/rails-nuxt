1561359ad52dbdd1d3400eed2493ec4c72e9e41f
tdkn <web@tedokon.com>
2019-03-19 19:20:05 +0900

    Using dotenv for configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "sample_project_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "#{ENV.fetch('APP_NAME', 'app_name')}_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
